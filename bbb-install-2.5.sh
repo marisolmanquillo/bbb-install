6a35653ab880a4efc306897914743b83504c0cc2
marisolmanquillo <marisolmanquillo08@gmail.com>
2023-02-06 18:44:03 -0500

    Update bbb-install-2.5.sh
    Elimino nodejs

--- bbb-install-2.5.sh
+++ bbb-install-2.5.sh
@@ -262,16 +262,7 @@ main() {
 
     rm -rf /etc/apt/sources.list.d/kurento.list     # Kurento 6.15 now packaged with 2.3
 
-    if grep -q 12 /etc/apt/sources.list.d/nodesource.list ; then # Node 12 might be installed, previously used in BigBlueButton
-      sudo apt-get purge nodejs
-      sudo rm -r /etc/apt/sources.list.d/nodesource.list
-    fi
-    if [ ! -f /etc/apt/sources.list.d/nodesource.list ]; then
-      curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    fi
-    if ! apt-cache madison nodejs | grep -q node_16; then
-      err "Did not detect nodejs 16.x candidate for installation"
-    fi
+
     if ! apt-key list MongoDB | grep -q 4.4; then
       wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
     fi
